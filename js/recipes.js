e7abd0a42c0686ee73529b9f0cf54ea2cea650e4
delta-minecraftdiary <delta.minecraftdiary@gmail.com>
2021-04-13 00:05:47 +0900

    modified recipe page

--- js/recipes.js
+++ js/recipes.js
@@ -1,62 +1,36 @@
-function getVersion() {
-    return "1.0"
-}
+window.addEventListener('load', function(){
+    addImgTag();
+});
 
-/*function getItemMap(ver) {
+function getJson(version) {
     let map = new Object();
     $.getJSON("../images/items/items.json", function(data) {
-        map = data[ver];
+        map = data[version];
     });
-    return map;
+    return map
 }
 
-function addItemImage() {
-    const ver = getVersion();
-    const parent = document.getElementById("inventory-items");
-    let nameMap = new Object();
-    const getJson = function () {
-        $.getJSON("../images/items/items.json", function (data) {
-            console.log(data[ver]);
-            nameMap = data[ver];
-        });
-    };
-    const getMap = function(callback) {
-        callback();
-    };
-    getMap(getJson);
-
-    // const nameMap = getItemMap(ver);
-    console.log(nameMap);
-    for (let index in nameMap) {
-        console.log(index);
-        let img = document.createElement("img");
-        img.className = "drag-image";
-        let expansion = "png";
-        if (nameMap[index] == "Golden Apple") expansion = "gif";
-        img.src = `../images/items/${ver}/${nameMap[index]}.${expansion}`;
-        parent.appendChild(img);
-    }
-}*/
-
-const getJSON = function(ver) {
-    let map = new Object();
-    $.getJSON("../images/items/items.json", function(data) {
-        map = data[ver];
-    });
-    return map;
-}
-
-const getItemMap = function(callback) {
-    return callback();
+function getVersion(){
+    return document.getElementById("version").value;
 }
 
 function addImgTag() {
-    const ver = getVersion();
-    const itemMap = getItemMap(function() {
-        getJSON(ver);
-    });
+    removeImg();
+    $.ajaxSetup({ async: false });
+    const version = getVersion();
+    const itemMap = getJson(version);
+    $.ajaxSetup({ async: true });
 
     for (let key in itemMap) {
-        $("#inventory-items").append(`<img class="drag-image" src="../images/items/${ver}/${key}.${extension}">`);
+        let extension = "png";
+        if (key == "Golden Apple") extension = "gif";
+        $("#inventory-items").append(`<img class="drag-image" src="../images/items/${version}/${key}.${extension}">`);
+    }
+}
+
+function removeImg(){
+    const parent = document.getElementById("inventory-items");
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
     }
 }
