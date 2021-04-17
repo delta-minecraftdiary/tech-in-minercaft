da13a0cf5b74c85c5fb28a474507d881faae46c2
delta-minecraftdiary <delta.minecraftdiary@gmail.com>
2021-04-18 00:35:17 +0900

    modified items.josn

--- recipes/test.py
+++ recipes/test.py
@@ -2,7 +2,19 @@ import json
 
 with open("images/items/items.json", encoding="UTF-8") as f:
     json_items = json.load(f)
-    json_items["1.16.5"][0]["obtaining"].append("crafting")
+
+new_json = {"1.16.5":[]}
+for element in json_items["1.16.5"]:
+    new_json["1.16.5"].append({
+        "id":element["id"],
+        "tab":element["tab"],
+        "group":element["group"],
+        "dimension":element["dimension"],
+        "obtaining":element["obtaining"],
+        "recipes":element["recipes"],
+        "usage":element["usage"],
+        "crafting":element["crafting"]
+    })
 
 with open("images/items/items.json", mode="w", encoding="UTF-8") as g:
-    json.dump(json_items, g)
+    json.dump(new_json, g)
